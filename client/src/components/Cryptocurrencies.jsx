64160e45ab8df23a2d67b5b8ffdb2582a242c512
jakubje <jakubjeczalik@gmail.com>
2023-09-25 23:05:18 +0100

    moved list coins to local api

--- client/src/components/Cryptocurrencies.jsx
+++ client/src/components/Cryptocurrencies.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 import millify from 'millify';
 import { Card, Row, Col, Input } from 'antd';
 import { RiseOutlined, FallOutlined } from '@ant-design/icons';
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptosQuery, useListCoinsQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
-  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  const { data: cryptoList, isFetching } = useListCoinsQuery(count);
+
+  // const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filteredData = cryptoList?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
     setCryptos(filteredData);
   }, [cryptoList, searchTerm]);
 
@@ -40,16 +42,16 @@ const Cryptocurrencies = ({ simplified }) => {
             sm={12}
             lg={6}
             className="crypto-card"
-            key={currency.uuid}
+            key={currency.coin_uuid}
           >
-            <Link to={`/crypto/${currency.symbol}/${currency.uuid}`}>
+            <Link to={`/crypto/${currency.coin_id}/${currency.coin_uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={
                   <img
                     className="crypto-image"
                     alt={`${currency.name}`}
-                    src={currency.iconUrl}
+                    src={currency.image_url}
                   />
                 }
                 hoverable="true"
@@ -57,9 +59,9 @@ const Cryptocurrencies = ({ simplified }) => {
               >
                 <div className="crypto-details">
                   <p>Price: {millify(currency.price)}</p>
-                  <p>Market Cap: {millify(currency.marketCap)}</p>
-                  <p>Daily Change: {millify(currency.change)}%</p>
-                  {millify(currency.change) < 0 ? (
+                  <p>Market Cap: {millify(currency.market_cap)}</p>
+                  <p>Daily Change: {millify(currency.daily_change)}%</p>
+                  {millify(currency.daily_change) < 0 ? (
                     <FallOutlined
                       className="crypto-change-symbol"
                       style={{ color: 'red' }}
