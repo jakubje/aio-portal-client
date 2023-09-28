e74cb77557a0eb949c15f71cc0c8d228797199dd
jakubje <jakubjeczalik@gmail.com>
2023-09-28 17:23:58 +0100

    Mutations for login/register/refresh token

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -13,7 +13,21 @@ export const authAPI = createApi({
         body: { email: email, password: password },
       }),
     }),
+    register: builder.mutation({
+      query: ({ email, name, last_name, password }) => ({
+        url: `/create_user`,
+        method: 'POST',
+        body: { email: email, name: name, last_name: last_name, password: password },
+      }),
+    }),
+    refreshToken: builder.mutation({
+      query: ({ refresh_token }) => ({
+        url: `/refresh_token`,
+        method: 'POST',
+        body: { refresh_token: refresh_token },
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation } = authAPI;
+export const { useLoginMutation, useRegisterMutation, useRefreshTokenMutation } = authAPI;
